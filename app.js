06550059a470dd1c2b47387808ff9240f852b65b
Andrew Blair <blair.d.andrew@gmail.com>
2019-12-16 17:11:35 +0000

    board is there about to go wild...

--- app.js
+++ app.js
@@ -3,18 +3,28 @@ let board = [];
 function createBoard(){
     for(i=0; i < 64; i++){
         if(i < 16){
-            board.push(1);
+            board.push('x');
         }   else if(i <48){
-            board.push(0);
+            board.push('o');
         } else{
-            board.push(2);
+            board.push('w');
         }
     }
     
 }
 createBoard();
-console.log(board)
+
 
 function renderBoard(){
-    document.createElement('')
+    let boardContainer = document.getElementById('board')
+    
+    for(element of board){
+        let gridSquare = document.createElement('div')
+        gridSquare.id = element
+        console.log(gridSquare)
+        boardContainer.appendChild(gridSquare);
+    }
+
 }
+renderBoard()
+console.log(board)

846ebba51a26f300849ba8df5476588cc827537b
JamesSwatton <jamessswatton@gmail.com>
2019-12-17 14:42:44 +0000

    add render pieces to board

--- app.js
+++ app.js
@@ -1,44 +1,59 @@
+let boardContainer = document.getElementById('board');
+let piecesContainer = document.getElementById('pieces-container');
+
 let board = [];
 
-function createBoard(){
-    for(i=0; i < 64; i++){
-        if(i < 16){
+function createBoard() {
+    for (i = 0; i < 64; i++) {
+        if (i < 16) {
             board.push('x');
-        }   else if(i <48){
+        } else if (i < 48) {
             board.push('o');
-        } else{
+        } else {
             board.push('w');
         }
     }
-    
 }
+
 createBoard();
 
 
-function renderBoard(){
-    let boardContainer = document.getElementById('board')
-    
-    for(i = 0; i < 8; i++){
-        for(j = 0; j < 8; j++){
-            if((i + j) % 2 === 0){
+function renderBoard() {
+
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            if ((i + j) % 2 === 0) {
                 let white = document.createElement('div');
-                white.id = "white"
-                console.log('true')
-                let circle = document.createElement('div')
-                circle.id ="circle";
-                white.appendChild(circle)
+                white.id = "white";
                 boardContainer.appendChild(white);
-            } else{
+            } else {
                 let black = document.createElement('div');
-                black.id = "black"
-                
+                black.id = "black";
+
                 boardContainer.appendChild(black);
             }
-        
+        }
     }
-       
-    }
-
 }
-renderBoard()
-console.log(board)
+
+function renderPieces() {
+    for (piece of board) {
+        if (piece == 'x') {
+            let whiteCircle = document.createElement('div');
+            whiteCircle.id = "whiteCircle";
+            piecesContainer.appendChild(whiteCircle);
+        } else if (piece == 'w') {
+            let blackCircle = document.createElement('div');
+            blackCircle.id = "blackCircle";
+            piecesContainer.appendChild(blackCircle);
+        } else {
+            let blank = document.createElement('div');
+            blank.id = "blank";
+            piecesContainer.appendChild(blank);
+        }
+    }
+}
+
+renderBoard();
+renderPieces();
+console.log(board);

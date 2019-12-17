514d6bb0e9bb50103af2be8e9e3039c61100806e
JamesSwatton <jamessswatton@gmail.com>
2019-12-17 15:41:34 +0000

    add position index capture

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let boardContainer = document.getElementById('board');
-let piecesContainer = document.getElementById('pieces-container');
+const boardContainer = document.getElementById('board');
+const piecesContainer = document.getElementById('pieces-container');
+let lastSelectedPositions = [0,0];
 
 let board = [];
 
@@ -40,20 +41,32 @@ function renderPieces() {
     for (let i = 0; i < board.length; i++) {
         if (board[i] == 'x') {
             let whiteCircle = document.createElement('div');
-            whiteCircle.id = "whiteCircle";
+            whiteCircle.className = "whiteCircle";
+            whiteCircle.id = `index-${i}`;
             piecesContainer.appendChild(whiteCircle);
         } else if (board[i] == 'w') {
             let blackCircle = document.createElement('div');
-            blackCircle.id = "blackCircle";
+            blackCircle.className = "blackCircle";
+            blackCircle.id = `index-${i}`;
             piecesContainer.appendChild(blackCircle);
         } else {
             let blank = document.createElement('div');
-            blank.id = "blank";
+            blank.className = "blank";
+            blank.id = `index-${i}`;
             piecesContainer.appendChild(blank);
         }
     }
 }
 
+function addPosition(){
+    piecesContainer.addEventListener('click', event => {
+        lastSelectedPositions.shift();
+        lastSelectedPositions.push(parseInt(event.target.id.split('-')[1]));
+        console.log('position', lastSelectedPositions);
+    });
+}
+
+addPosition();
 renderBoard();
 renderPieces();
 console.log(board);

bddf60633030cc89ee296b9ff28ab12ece758fb9
Andrew Blair <blair.d.andrew@gmail.com>
2019-12-18 16:07:12 +0000

    added new js file. added turns for player moves

--- app.js
+++ app.js
@@ -1,74 +1,3 @@
-const boardContainer = document.getElementById('board');
-let piecesContainer = document.getElementById('pieces-container');
-let lastSelectedPositions = [];
-
-let board = [];
-
-function createBoard() {
-    for (i = 0; i < 64; i++) {
-        if (i < 16) {
-            board.push('x');
-        } else if (i < 48) {
-            board.push('o');
-        } else {
-            board.push('w');
-        }
-    }
-}
-
-createBoard();
-
-
-function renderBoard() {
-
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-            if ((i + j) % 2 === 0) {
-                let white = document.createElement('div');
-                white.id = "white";
-                boardContainer.appendChild(white);
-            } else {
-                let black = document.createElement('div');
-                black.id = "black";
-
-                boardContainer.appendChild(black);
-            }
-        }
-    }
-}
-
-function renderPieces() {
-    for (let i = 0; i < board.length; i++) {
-        if (board[i] == 'x') {
-            let whiteCircle = document.createElement('div');
-            whiteCircle.className = "whiteCircle";
-            whiteCircle.id = `index-${i}`;
-            piecesContainer.appendChild(whiteCircle);
-        } else if (board[i] == 'w') {
-            let blackCircle = document.createElement('div');
-            blackCircle.className = "blackCircle";
-            blackCircle.id = `index-${i}`;
-            piecesContainer.appendChild(blackCircle);
-        } else {
-            let blank = document.createElement('div');
-            blank.className = "blank";
-            blank.id = `index-${i}`;
-            piecesContainer.appendChild(blank);
-        }
-    }
-}
-
-function swapPositions(positionArray) {
-    let pos1 = board[positionArray[0]];
-    let pos2 = board[positionArray[1]];
-    console.log('pos1', pos1);
-    console.log('pos2', pos2);
-    board[positionArray[1]] = pos1;
-    board[positionArray[0]] = pos2;
-
-    console.log('last clicked pos', board[positionArray[1]]);
-}
-
 piecesContainer.addEventListener('click', event => {
     if (lastSelectedPositions.length < 2) {
         lastSelectedPositions.push(parseInt(event.target.id.split('-')[1]));
@@ -84,12 +13,15 @@ piecesContainer.addEventListener('click', event => {
     console.log('position', lastSelectedPositions);
 });
 
-function updatePieces() {
-    piecesContainer.innerHTML = '';
-    renderPieces();
-}
-
-// addPosition();
-renderBoard();
-renderPieces();
-// console.log(board);
+document.getElementById('turn-change').addEventListener('click', event =>{
+    if(game.activePlayer === player1){
+        game.activePlayer = player2
+        document.getElementById('active-player').style['background-color'] 
+        = 'green'
+    } else {
+        game.activePlayer = player1
+        document.getElementById('active-player').style['background-color'] 
+        = 'red'
+    } 
+    console.log(game.activePlayer.name)
+})
---
+++ board.js
@@ -0,0 +1,89 @@
+const player1 = {name: "James"};
+const player2 = {name: "Andrew"};
+// let players =[player1, player2];
+const game = {activePlayer: player1}
+
+
+const boardContainer = document.getElementById('board');
+let piecesContainer = document.getElementById('pieces-container');
+let lastSelectedPositions = [];
+
+let board = [];
+
+function createBoard() {
+    for (i = 0; i < 64; i++) {
+        if (i < 16) {
+            board.push('x');
+        } else if (i < 48) {
+            board.push('o');
+        } else {
+            board.push('w');
+        }
+    }
+}
+
+createBoard();
+
+function renderBoard() {
+
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            if ((i + j) % 2 === 0) {
+                let white = document.createElement('div');
+                white.id = "white";
+                boardContainer.appendChild(white);
+            } else {
+                let black = document.createElement('div');
+                black.id = "black";
+
+                boardContainer.appendChild(black);
+            }
+        }
+    }
+}
+
+function renderPieces() {
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] == 'x') {
+            let redCircle = document.createElement('div');
+            redCircle.className = "redCircle";
+            redCircle.id = `index-${i}`;
+            piecesContainer.appendChild(redCircle);
+        } else if (board[i] == 'w') {
+            let greenCircle = document.createElement('div');
+            greenCircle.className = "greenCircle";
+            greenCircle.id = `index-${i}`;
+            piecesContainer.appendChild(greenCircle);
+        } else {
+            let blank = document.createElement('div');
+            blank.className = "blank";
+            blank.id = `index-${i}`;
+            piecesContainer.appendChild(blank);
+        }
+    }
+}
+
+function swapPositions(positionArray) {
+    let pos1 = board[positionArray[0]];
+    let pos2 = board[positionArray[1]];
+    console.log('pos1', pos1);
+    console.log('pos2', pos2);
+    board[positionArray[1]] = pos1;
+    board[positionArray[0]] = pos2;
+
+    console.log('last clicked pos', board[positionArray[1]]);
+}
+
+function updatePieces() {
+    piecesContainer.innerHTML = '';
+    renderPieces();
+}
+
+// addPosition();
+renderBoard();
+renderPieces();
+// console.log(board);
+
+function playerTurn(){
+    
+}

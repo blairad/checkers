4c3e8d61f027ee6dd40990f5ef8d06291855e2ee
JamesSwatton <jamessswatton@gmail.com>
2019-12-19 15:59:30 +0000

    refactored canTakePiece

--- gameLogic.js
+++ gameLogic.js
@@ -12,14 +12,14 @@ function canTakePiece(){
 
     if(game.activePlayer.colour === 'redCircle'){
         return (lastSelectedPositions[1] - 18 === lastSelectedPositions[0] 
-            && board[lastSelectedPositions[1] - 9] === 'w')
+            && board[lastSelectedPositions[1] - 9] === 'g')
             || (lastSelectedPositions[1] - 14 === lastSelectedPositions[0]
-            && board[lastSelectedPositions[1] - 7] === 'w')
+            && board[lastSelectedPositions[1] - 7] === 'g')
     } else if (game.activePlayer.colour === 'greenCircle'){
         return (lastSelectedPositions[1] + 18 === lastSelectedPositions[0]
-            && board[lastSelectedPositions[1] + 9] === 'x')
+            && board[lastSelectedPositions[1] + 9] === 'r')
             || (lastSelectedPositions[1] + 14 === lastSelectedPositions[0]
-            && board[lastSelectedPositions[1] + 7] === 'x')
+            && board[lastSelectedPositions[1] + 7] === 'r')
     }
 }
  

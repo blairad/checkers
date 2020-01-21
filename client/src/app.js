780760f1efb49b3f9c5ea560159b0d4404ca602e
JamesSwatton <jamessswatton@gmail.com>
2020-01-21 13:45:26 +0000

    add restrict player to move own piece

--- client/src/app.js
+++ client/src/app.js
@@ -29,7 +29,6 @@ window.onload = () => {
     console.table(game.board.pieces);
 
     document.getElementById('new-game').onclick = () => {
-        console.log('butts');
         game.board.clearCapturePos();
         game.board.setupPieces();
         game.activePlayer = 1;
@@ -78,48 +77,58 @@ window.onload = () => {
 
     // this is where the players 'turn' logic happens
     document.getElementById('pieces-container').addEventListener('click', event => {
-        const clickPosition = parseInt(event.target.id.split('-')[1]);
-        console.log(clickPosition);
+        const clickPosition = +event.target.id.split('-')[1];
+        let playerSelectedPieceNum = game.board.pieces[clickPosition].player;
 
-        game.addMovePosition(clickPosition);
 
-        if (game.hasSelectedPieceAndMovePos()) {
-            if (game.board.canCapture ) {
-                if(game.board.isValidCaptureMove(game.move)) {
-                    game.board.capturePiece(game.move, game.getOpponent());
-                    game.board.clearCaptureAndMovePos();
-                    game.board.calcMovePositions(game.activePlayer);
-                    game.board.calcCapturePositions(game.activePlayer, game.opponent);
-                    if (!game.board.canCapture) {
-                        game.switchPlayer();
+        console.log('you are player: ', youArePlayer);
+        console.log('selected piece player: ', playerSelectedPieceNum);
+        console.log('click position: ', clickPosition);
+
+        // can only make move if player has selected own piece
+        if (playerSelectedPieceNum === youArePlayer || playerSelectedPieceNum === 0) {
+            console.log('youre in!');
+
+            game.addMovePosition(clickPosition);
+
+            if (game.hasSelectedPieceAndMovePos()) {
+                if (game.board.canCapture ) {
+                    if(game.board.isValidCaptureMove(game.move)) {
+                        game.board.capturePiece(game.move, game.getOpponent());
                         game.board.clearCaptureAndMovePos();
                         game.board.calcMovePositions(game.activePlayer);
                         game.board.calcCapturePositions(game.activePlayer, game.opponent);
+                        if (!game.board.canCapture) {
+                            game.switchPlayer();
+                            game.board.clearCaptureAndMovePos();
+                            game.board.calcMovePositions(game.activePlayer);
+                            game.board.calcCapturePositions(game.activePlayer, game.opponent);
+                            gameView.renderActivePlayer(game.activePlayer);
+                            boardView.renderPieces(game.board.pieces);
+                        }
                         gameView.renderActivePlayer(game.activePlayer);
                         boardView.renderPieces(game.board.pieces);
+                        socket.emit('pieces', game.board.pieces);
                     }
-                    gameView.renderActivePlayer(game.activePlayer);
-                    boardView.renderPieces(game.board.pieces);
-                    socket.emit('pieces', game.board.pieces);
-                }
-            } else if (game.board.canMove) {
-                console.log(game.board.isValidMove(game.move));
-                if (game.board.isValidMove(game.move)){
-                    game.board.movePiece(game.move);
-                    console.log('active player: ', game.activePlayer);
-                    game.switchPlayer();
-                    console.log('active player: ', game.activePlayer);
-                    game.board.clearCaptureAndMovePos();
-                    game.board.calcMovePositions(game.activePlayer);
-                    game.board.calcCapturePositions(game.activePlayer, game.opponent);
-                    console.log(game.activePlayer);
-                    gameView.renderActivePlayer(game.activePlayer);
-                    console.log("valid move");
-                    boardView.renderPieces(game.board.pieces);
-                    socket.emit('pieces', game.board.pieces);
+                } else if (game.board.canMove) {
+                    console.log(game.board.isValidMove(game.move));
+                    if (game.board.isValidMove(game.move)){
+                        game.board.movePiece(game.move);
+                        console.log('active player: ', game.activePlayer);
+                        game.switchPlayer();
+                        console.log('active player: ', game.activePlayer);
+                        game.board.clearCaptureAndMovePos();
+                        game.board.calcMovePositions(game.activePlayer);
+                        game.board.calcCapturePositions(game.activePlayer, game.opponent);
+                        console.log(game.activePlayer);
+                        gameView.renderActivePlayer(game.activePlayer);
+                        console.log("valid move");
+                        boardView.renderPieces(game.board.pieces);
+                        socket.emit('pieces', game.board.pieces);
                     }
                 }
             }
+        }
         console.log('piece count: ', game.getActivePlayer().pieceCount);
         console.log(`player ${game.checkForWinner()} is the winner!`);
         // console.log(game.board.canCapture);

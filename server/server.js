520d59b150a02abedf298dd49e144ea9305fc5da
Andrew Blair <blair.d.andrew@gmail.com>
2020-01-27 13:21:22 +0000

    assign game with random numbers working and change of in-game css

--- server/server.js
+++ server/server.js
@@ -17,35 +17,58 @@ server.listen(port, () => {
 
 io.on('connection', (socket) => {
     // initial form submition 
-    socket.on('joinLobby', (player) =>{
+    socket.on('joinLobby', (player) => {
         players[player.id] = player;
         io.emit('playerList', players)
     });
 
     // change player status and create game obj
-    socket.on('playerReady', playerId =>{
+    socket.on('playerReady', playerId => {
+        // console.log(players[playerId])
         players[playerId].gameStatus = 'ready';
-        players[playerId].playerNum = Math.floor(Math.random() * (3 - 1) + 1);
+        createNewGame(playerId);
+        // console.log(games);
+        // console.log(games[playerId][playerId])
+        // games[playerId][playerId][playerNum] = Math.floor(Math.random() * (3 - 1) + 1)
         io.emit('playerList', players);
-        games[playerId] = {
-            player1: players[playerId]
-        }
-        console.log(players)
+
+        // console.log(players) 
     });
 
     // add player2 to game
     socket.on('pairPlayers', (playerId, gameId) => {
-        games[gameId].player2 = players[playerId];
-        players[playerId].playerNum = () => {
-            return games[gameId][gameId].playerNum === 1 ? 2 : 1;
-        };
-        socket.emit('game', games[gameId] )
-    } )
+        players[playerId].gameStatus = 'in-game';
+        players[gameId].gameStatus = 'in-game';
+        games[gameId].push(players[playerId]);
+        // players[playerId].playerNum = () => {
+        //     return games[gameId][gameId].playerNum === 1 ? 2 : 1;
+        // };
+        assignPlayerNums(games[gameId])
+        console.log(games)
+        io.emit('playerList', players)
+        socket.emit('game', games[gameId])
+    })
 
     // send pieces on player turn 
     socket.on('pieces', (opponentId, pieces) => {
         socket.broadcast.to(opponentId).emit('pieces', pieces);
     })
-    
+
+    function createNewGame(playerId) {
+        games[playerId] = [
+            players[playerId]
+        ]
+    }
+
+    function assignPlayerNums(game) {
+        const player1RandomNum = Math.floor(Math.random() * (3 - 1) + 1);
+        const player2RandomNum = player1RandomNum === 1 ? 2 : 1;
+        
+        const randomNums = [player1RandomNum, player2RandomNum];
+
+        for (let i = 0; i < game.length; i++) {
+            game[i].playerNum = randomNums[i];
+        }
+    }
 
 });
